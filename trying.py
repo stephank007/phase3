543a90330c98882daef1702b95fcd9832bce6fe0
stephank007 <stephank@orantech.co.il>
2021-06-11 22:22:28 +0300

    update new trying marimekko chart

--- trying.py
+++ trying.py
@@ -14,6 +14,10 @@ path = config['config'].get('path')
 webp = config['config'].get('web')
 fn = os.path.join(path, './_out/stat-file.xlsx')
 fo = os.path.join(path, './_out/stat-file.html')
+f1 = os.path.join(path, './_out/stat-file-out.xlsx')
+
+writer   = pd.ExcelWriter(f1, engine='xlsxwriter')
+workbook = writer.book
 
 df = pd.read_excel(fn, sheet_name='Sheet1')
 df['month'] = pd.to_datetime(df['Finish_Date']).dt.strftime('%Y-%m')
@@ -24,10 +28,14 @@ df_g = df.groupby(['month']).agg({
     'completed': 'sum'
 })
 df_g.reset_index(inplace=True)
+gt = df_g['total'].sum()
 df_g['month-label'] = pd.to_datetime(df_g['month'], format='%Y-%m').dt.strftime('%b-%Y')
-df_g['in-dev'] = ( df_g['in-dev'] / df_g['total'] ) * 100
-df_g['completed'] = ( df_g['completed'] / df_g['total'] ) * 100
-df_g['width'] = ( df_g['total'] / df_g['total'].sum() ) * 100
+df_g['% in-dev'] = ( df_g['in-dev'] / df_g['total'] ) * 100
+df_g['% completed'] = ( df_g['completed'] / df_g['total'] ) * 100
+df_g['width'] = ( df_g['total'] / gt ) * 100
+
+df_g.to_excel(writer, sheet_name='Sheet1')
+writer.save()
 
 # labels = ["apples","oranges","pears","bananas"]
 labels = df_g['month-label']
@@ -35,12 +43,9 @@ labels = df_g['month-label']
 widths = np.array(df_g['width'])
 
 data = {
-    "completed": df_g['completed'].astype(int),
-    "in-dev"   : df_g['in-dev'].astype(int),
+    "completed"  : df_g['% completed'].apply(np.ceil),
+    "in-dev"     : df_g['% in-dev'].apply(np.floor),
 }
-
-customdata = [labels.to_list(), df_g['in-dev'].to_list()]
-print(customdata)
 
 fig = go.Figure()
 for key in data:
@@ -48,20 +53,19 @@ for key in data:
         name=key,
         y=data[key],
         x=np.cumsum(widths)-widths,
+        #x=labels.to_list(),
         width=widths,
         offset=0,
         customdata=np.transpose([labels, data[key]]),
+        text='# in-dev: ' + df_g['in-dev'].astype(str) + '<br>' + '# completed: ' + df_g['completed'].astype(str),
         texttemplate="%{y} ",
-        #texttemplate="%{y} x %{width} =<br>%{customdata[0]}",
-        # textposition="inside",
-        textposition="auto",
+        textposition="inside",
         textangle=0,
         hovertemplate="<br>".join([
-            "month: %{customdata[0]}",
-            # "width: %{width}",
-            "requirements: %{y}",
-            #"area: %{customdata[1]}",
-        ])
+            "%{customdata[0]}",
+            key + ": %{y}%",
+            "%{text}"
+        ]),
     ))
 
 fig.update_xaxes(
@@ -74,7 +78,7 @@ fig.update_xaxes(range=[0,100])
 fig.update_yaxes(range=[0,100])
 
 fig.update_layout(
-    title_text="Marimekko Chart",
+    title_text="% in-progress vs. completed - Marimekko Chart",
     barmode="stack",
     uniformtext=dict(mode="hide", minsize=10),
     xaxis={'categoryorder': 'category ascending'}
