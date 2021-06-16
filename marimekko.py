541da8ccdaa8ad86cbcd0aa319e1a432eed3f568
stephank007 <stephank@orantech.co.il>
2021-06-16 16:43:03 +0300

    1. add vrect to marimekko.py chart

--- marimekko.py
+++ marimekko.py
@@ -32,9 +32,8 @@ df_g['month-label'] = pd.to_datetime(df_g['month'], format='%Y-%m').dt.strftime(
 df_g['% open']      = ( df_g['open'] / df_g['total'] )   * 100
 df_g['% closed']    = ( df_g['closed'] / df_g['total'] ) * 100
 df_g['width']       = ( df_g['total'] / gt )             * 100
-
-df_g.to_excel(writer, sheet_name='Sheet1')
-writer.save()
+df_g['cumsum'] = df_g['width'].cumsum().shift(1)
+df_g.loc[0, 'cumsum'] = 0
 
 labels = df_g['month-label']
 widths = np.array(df_g['width'])
@@ -50,13 +49,23 @@ colors = {
 customdata = np.transpose([labels, data['closed']]),
 print('length --> ', len(customdata))
 df_x = pd.DataFrame(customdata)
+today    = pd.to_datetime('today')
+t_yr = today.year
+t_mn = 1 if today.month + 1 > 12 else today.month + 1
+x0_rect = '{}-{}'.format(t_yr, f'{t_mn:0>2}')
+x1_rect = df_g['month'].max()
+
+x0_vrect = df_g[df_g['month'] == x0_rect]['cumsum'].values[0]
+x1_vrect = df_g[df_g['month'] == x1_rect]['cumsum'].values[0] + df_g[df_g['month'] == x1_rect]['width'].values[0]
+
 
 fig = go.Figure()
 for key in data:
     fig.add_trace(go.Bar(
         name=key,
         y=data[key],
-        x=np.cumsum(widths)-widths,
+        x=df_g['cumsum'],
+        #x=np.cumsum(widths)-widths,
         #x=labels.to_list(),
         width=widths,
         marker_color=colors[key],
@@ -80,6 +89,12 @@ fig.update_xaxes(
     #ticktext= ["%s<br>%d" % (l, w) for l, w in zip(labels, widths)]
     ticktext=["%s" % (l) for l, w in zip(labels, widths)],
 )
+
+fig.add_vrect(
+    x0=x0_vrect, x1=x1_vrect,
+    fillcolor="LightSalmon", opacity=0.8,
+    layer="above", line_width=0,
+),
 
 fig.update_xaxes(range=[0, 100])
 fig.update_yaxes(range=[0, 100])
