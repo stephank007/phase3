74d99f9f41936662d12d2ee984398de7593c905c
stephank007 <stephank@orantech.co.il>
2021-08-13 17:39:14 +0300

    new baseline

--- trying.py
+++ trying.py
@@ -6,13 +6,10 @@ import warnings as warning
 import yaml
 import plotly.express as px
 import plotly.graph_objects as go
-import dash_core_components as dcc
-import dash_html_components as html
-import dash_bootstrap_components as dbc
-import dash_table as dt
-import r_heatmap
-import r_stacked
+from plotly.subplots import make_subplots
 import plotly.offline as pyo
+
+pc = '% Complete'
 
 pd.options.mode.chained_assignment = 'raise'
 warning.filterwarnings('ignore')
@@ -71,12 +68,75 @@ labels = ["World"] + region_wise_pop["Region"].values.tolist() + world_countries
 values = [world_countries_data["Population"].sum()] + region_wise_pop["Population"].values.tolist() + \
          world_countries_data["Population"].values.tolist()
 
-fig = go.Figure(go.Sunburst(
-    parents=parents,
-    labels=labels,
-    values=values,
-))
 
-fig.update_layout(title="World Population Per Country Per Region",
-                  width=700, height=700)
+#print(df.head())
+
+def build_hierarchical_dataframe(df, levels, value_column, color_columns=None):
+    """
+    Build a hierarchy of levels for Sunburst or Treemap charts.
+
+    Levels are given starting from the bottom to the top of the hierarchy,
+    ie the last level corresponds to the root.
+    """
+    df_all_trees = pd.DataFrame(columns=['id', 'parent', 'value', 'color'])
+    for i, level in enumerate(levels):
+        df_tree = pd.DataFrame(columns=['id', 'parent', 'value', 'color'])
+        dfg = df.groupby(levels[i:]).sum()
+        dfg = dfg.reset_index()
+        df_tree['id'] = dfg[level].copy()
+        if i < len(levels) - 1:
+            df_tree['parent'] = dfg[levels[i+1]].copy()
+        else:
+            df_tree['parent'] = 'total'
+        df_tree['value'] = dfg[value_column]
+        df_tree['color'] = dfg[color_columns[0]] / dfg[color_columns[1]]
+        df_all_trees = df_all_trees.append(df_tree, ignore_index=True)
+    total = pd.Series(dict(id='total', parent='',
+                           value=df[value_column].sum(),
+                           color=df[color_columns[0]].sum() / df[color_columns[1]].sum()))
+    df_all_trees = df_all_trees.append(total, ignore_index=True)
+    return df_all_trees
+
+# df['completed']  = df[pc].apply(lambda x: 1 if x == 1.0 else 0.0001)
+df = df.groupby(by=['rule', 'domain_id', 'process_id']).sum()[['value', 'Duration']].reset_index()
+#df = df[['rule', 'domain_id', 'process_id', 'value', 'Duration']].reset_index()
+#df.sort_values(['rule', 'domain_id', 'process_id'], inplace=True)
+
+levels = ['process_id', 'domain_id', 'rule']  # levels used for the hierarchical chart
+color_columns = ['value', 'Duration']
+value_column = 'value'
+#
+# df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/sales_success.csv')
+# print(df.head())
+#
+# levels = ['salesperson', 'county', 'region'] # levels used for the hierarchical chart
+# color_columns = ['sales', 'calls']
+# value_column = 'calls'
+
+df_all_trees = build_hierarchical_dataframe(df, levels, value_column, color_columns)
+# average_score = df['sales'].sum() / df['calls'].sum()
+average_score = df['value'].sum() / df['Duration'].sum()
+##############################################################
+fig = go.Figure()
+fig.add_trace(go.Sunburst(
+    labels=df_all_trees['id'],
+    parents=df_all_trees['parent'],
+    values=df_all_trees['value'],
+    branchvalues='total',
+    marker=dict(
+        colors=df_all_trees['color'],
+        colorscale='RdBu',
+        cmid=average_score),
+    hovertemplate='<b>%{label} </b> <br> value: %{value}<br> Duration: %{color:.2f}',
+    name=''
+    )
+)
+
+fig.update_traces(insidetextorientation='radial')
+fig.update_layout(
+    title="process weight distribution",
+    width=1000,
+    height=1000,
+    uniformtext=dict(minsize=10, mode='hide')
+)
 pyo.plot(fig, filename=fo)
